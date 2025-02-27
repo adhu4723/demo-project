fa3e0ce486ae0168ce3b16ff59c9a5a7f6e2a10e
adhu4723 <adarshdhanwis@gmail.com>
2025-02-27 16:41:58 +0530

    Create functions.js

---
+++ functions.js
@@ -0,0 +1,44 @@
+// Funtions
+
+// 1) Function Declaration (Regular Function)
+
+// function greet(name) {
+//     return `Hello, ${name}!`;
+// }
+
+// console.log(greet("Alice")); // Output: Hello, Alice!
+
+// 2) Function Expression
+// const greet = function(name) {
+//     return `Hello, ${name}!`;
+// };
+
+// console.log(greet("Bob")); // Output: Hello, Bob!
+
+// 3) Arrow Functions (ES6)
+
+// const greet = (name) => {
+//     return `Hello, ${name}!`;
+// };
+
+// console.log(greet("Charlie")); // Output: Hello, Charlie!
+
+//  Shortern Arrow Function
+
+// const greet = name => `Hello, ${name}!`;
+// console.log(greet("David")); // Output: Hello, David!
+
+
+
+// 4. Function Parameters and Default Values
+// const greet = (name = "Guest") => `Hello, ${name}!`;
+
+// console.log(greet()); // Output: Hello, Guest!
+// console.log(greet("Eve")); // Output: Hello, Eve! 
+
+
+// Questions 
+// Write a function addNumbers(a, b) that takes two numbers as input and returns their sum.
+// Write a function toCelsius(fahrenheit) that converts Fahrenheit to Celsius using the formula:
+// (fahrenheit - 32) * 5 / 9;
+// Write a function isEven(number) that returns "Even" if the number is even, and "Odd" if the number is odd.
