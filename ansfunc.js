d90303aad8f44c4a18f1d0d8b8bc328c06fff405
adhu4723 <adarshdhanwis@gmail.com>
2025-02-28 08:51:17 +0530

    Create ansfunc.js

---
+++ ansfunc.js
@@ -0,0 +1,9 @@
+function toCelsius(fahrenheit) {
+    return (fahrenheit - 32) * 5 / 9;
+}
+
+// Example usage:
+console.log(toCelsius(100)); // Output: 37.77777777777778
+console.log(toCelsius(32));  // Output: 0
+console.log(toCelsius(0));   // Output: -17.77777777777778
+
